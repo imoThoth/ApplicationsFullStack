e1155c24787ab6a581fcfd6b3a8f7f82d32598be
unknown <imothoth@gmail.com>
2022-01-26 06:32:29 +0000

    first commit

---
+++ athena/src/main/java/com/cupcakes/athena/AthenaApplication.java
@@ -0,0 +1,25 @@
+package com.cupcakes.athena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class AthenaApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenaApplication.class, args);
+	}
+
+}
+
