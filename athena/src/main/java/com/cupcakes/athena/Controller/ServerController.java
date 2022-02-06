615ba16f9b98491addc2f5a642f3afb184356787
unknown <imothoth@gmail.com>
2022-02-06 12:48:55 +0000

    User UDT

--- athena/src/main/java/com/cupcakes/athena/Controller/ServerController.java
+++ athena/src/main/java/com/cupcakes/athena/Controller/ServerController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping("/")
+@RequestMapping(path = "database")
 @CrossOrigin
 @ResponseBody
 public class ServerController {
 
-    @GetMapping()
+    @GetMapping(path = "/user")
     public String helloWorld(){
         return "Hello To The New User";
     }
