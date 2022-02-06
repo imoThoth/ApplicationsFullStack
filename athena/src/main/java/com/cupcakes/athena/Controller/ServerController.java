f33922af77dda35d90bfcff174a8a9e69732416d
unknown <imothoth@gmail.com>
2022-02-06 23:38:46 +0000

    Cors Error

--- athena/src/main/java/com/cupcakes/athena/Controller/ServerController.java
+++ athena/src/main/java/com/cupcakes/athena/Controller/ServerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequestMapping()
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 public class ServerController {
 
