63ef08d629cfdc3e9b09321f979c0ed15db4d879
unknown <imothoth@gmail.com>
2022-02-06 12:54:18 +0000

    UDT controller

--- athena/src/main/java/com/cupcakes/athena/Controller/ServerController.java
+++ athena/src/main/java/com/cupcakes/athena/Controller/ServerController.java
@@ -1,13 +1,10 @@
 package com.cupcakes.athena.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(path = "database")
-@CrossOrigin
-@ResponseBody
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
 public class ServerController {
 
     @GetMapping(path = "/user")
